e4a8dd23eaed36a101b7c59d5feff3dc7314420d
sorokinaolga <sorokinaop@list.ru>
2021-12-26 19:51:38 +0300

    Инициализация проекта

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <FormUserCreate />
+  </div>
+</template>
+
+<script>
+import FormUserCreate from './components/form-user-create.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FormUserCreate
+  }
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+  margin-top: 60px
+</style>
---
+++ src/components/form-user-create.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Регистрация нового клиента</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'FormUserCreate',
+}
+</script>
+
+<style lang="sass">
+$secondary-color: #336789
+
+h1
+  background-color: $secondary-color
+</style>
