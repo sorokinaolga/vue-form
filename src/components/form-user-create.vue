b38318aac41fd6d944259e89416cb116da4e5c77
sorokinaolga <sorokinaop@list.ru>
2021-12-28 14:19:34 +0300

    Добавление данных в форму

--- src/components/form-user-create.vue
+++ src/components/form-user-create.vue
@@ -4,36 +4,81 @@
     <h2>Личные данные</h2>
     <div class="user-form__item">
       <label for="surname">Фамилия*</label>
-      <input type="text" id="surname" placeholder="" />
+      <input 
+        type="text" 
+        id="surname" 
+        placeholder="" 
+        v-model.trim="user.surname"
+        required 
+      />
     </div>
     <div class="user-form__item">
       <label for="name">Имя*</label>
-      <input type="text" id="name" placeholder="" />
+      <input 
+        type="text" 
+        id="name" 
+        placeholder=""
+        v-model.trim="user.name"
+        required
+      />
     </div>
     <div class="user-form__item">
       <label for="patronym">Отчество</label>
-      <input type="text" id="patronym" placeholder="" />
+      <input 
+        type="text" 
+        id="patronym" 
+        placeholder=""
+        v-model.trim="user.patronym"
+      />
     </div>
     <div class="user-form__item">
       <label for="birthdate">Дата рождения*</label>
-      <input type="date" id="birthdate" />
+      <input 
+        type="date" 
+        id="birthdate"
+        v-model="user.birthdate"
+        required
+      />
     </div>
     <div class="user-form__item">
       <label for="phone">Номер телефона*</label>
-      <input type="tel" id="phone" placeholder="" />
+      <input 
+        type="tel" 
+        id="phone" 
+        placeholder=""
+        v-model="user.phone"
+        required
+      />
     </div>
     <div class="user-form__item">
       <span class="label">Пол</span>
       <div class="user-form__box">
-        <input type="radio" id="man" name="gender" value="man" />
+        <input 
+          type="radio" 
+          id="man" 
+          name="gender" 
+          value="man"
+          v-model="user.gender"
+        />
         <label for="man">Мужской</label>
-        <input type="radio" id="woman" name="gender" value="woman" />
+        <input 
+          type="radio" 
+          id="woman" 
+          name="gender" 
+          value="woman"
+          v-model="user.gender"
+        />
         <label for="woman">Женский</label>
       </div>
     </div>
     <div class="user-form__item">
       <label for="customer-group">Группа клиентов*</label>
-      <select id="customer-group" multiple size="1">
+      <select 
+        id="customer-group" 
+        multiple 
+        v-model="user.customerGroup"
+        required
+      >
         <option value="vip">VIP</option>
         <option value="trouble">Проблемные</option>
         <option value="insured">ОМС</option>
@@ -41,67 +86,138 @@
     </div>
     <div class="user-form__item">
       <label for="doctor">Лечащий врач</label>
-      <select id="doctor">
-        <option value="Ivanov">Иванов</option>
-        <option value="Zaharov">Захаров</option>
-        <option value="Chernyshova">Чернышева</option>
+      <select 
+        id="doctor" 
+        v-model="user.doctor"
+      >
+        <option disabled value="">Фамилия врача</option>
+        <option
+          v-for="doctor in doctors"
+          :key="doctor" 
+          :value="doctor"
+        >
+          {{ doctor }}
+        </option>
       </select>
     </div>
     <div class="user-form__box">
-      <input type="checkbox" id="no-send-sms">
+      <input 
+        type="checkbox" 
+        id="no-send-sms" 
+        v-model="user.noSendSms"
+      >
       <label for="no-send-sms">Не отправлять СМС</label>
     </div>
 
     <h2>Адрес</h2>
     <div class="user-form__item">
       <label for="post-code">Индекс</label>
-      <input type="number" id="post-code" placeholder="" />
+      <input 
+        type="number" 
+        id="post-code" 
+        placeholder="" 
+        v-model="user.address.postCode"
+      />
     </div>
     <div class="user-form__item">
       <label for="country">Страна</label>
-      <input type="text" id="country" placeholder="" />
+      <input 
+        type="text" 
+        id="country" 
+        placeholder="" 
+        v-model.trim="user.address.country"
+      />
     </div>
     <div class="user-form__item">
       <label for="area">Область</label>
-      <input type="text" id="area" placeholder="" />
+      <input 
+        type="text" 
+        id="area" 
+        placeholder="" 
+        v-model.trim="user.address.area"
+      />
     </div>
     <div class="user-form__item">
       <label for="city">Город*</label>
-      <input type="text" id="city" placeholder="" />
+      <input 
+        type="text" 
+        id="city" 
+        placeholder="" 
+        v-model.trim="user.address.city"
+        required
+      />
     </div>
     <div class="user-form__item">
       <label for="street">Улица</label>
-      <input type="text" id="street" placeholder="" />
+      <input 
+        type="text" 
+        id="street" 
+        placeholder="" 
+        v-model.trim="user.address.street"
+      />
     </div>
     <div class="user-form__item">
       <label for="house">Дом</label>
-      <input type="text" id="house" placeholder="" />
+      <input 
+        type="text" 
+        id="house" 
+        placeholder="" 
+        v-model.trim="user.address.house"
+      />
     </div>
 
     <h2>Удостоверяющий документ</h2>
     <div class="user-form__item">
       <label for="document-type">Тип документа</label>
-      <select id="document-type">
-        <option value="passport">Паспорт</option>
-        <option value="birth-certificate">Свидетельство о рождении</option>
-        <option value="driving-permit">Вод. удостоверение</option>
+      <select 
+        id="document-type" 
+        v-model="user.document.type"
+      >
+        <option disabled value="">Тип документа</option>
+        <option
+          v-for="type in documentTypes"
+          :key="type" 
+          :value="type"
+        >
+          {{ type }}
+        </option>
       </select>
     </div>
     <div class="user-form__item">
       <label for="series">Серия</label>
-      <input type="number" id="series" placeholder="" />
+      <input 
+        type="number" 
+        id="series" 
+        placeholder="" 
+        v-model="user.document.series"
+      />
     </div>
     <div class="user-form__item">
       <label for="number">Номер</label>
-      <input type="number" id="number" placeholder="" />
+      <input 
+        type="number" 
+        id="number" 
+        placeholder="" 
+        v-model="user.document.number"
+      />
     </div>
     <div class="user-form__item">
       <label for="issued-by-whom">Кем выдан</label>
-      <input type="text" id="issued-by-whom" placeholder="" />
+      <input 
+        type="text" 
+        id="issued-by-whom" 
+        placeholder="" 
+        v-model.trim="user.document.issuedByWhom"
+      />
     </div>
     <div class="user-form__item">
       <label for="date-of-issue">Дата выдачи*</label>
-      <input type="date" id="date-of-issue" />
+      <input 
+        type="date" 
+        id="date-of-issue" 
+        v-model="user.document.dateOfIssue"
+        required
+      />
     </div>
 
     <button type="submit" class="btn">Зарегистрировать</button>
@@ -109,9 +225,39 @@
 </template>
 
 <script>
-
 export default {
-  name: 'FormUserCreate',
+  data() {
+    return {
+      doctors: ["Иванов", "Захаров", "Чернышева"],
+      documentTypes: ["Паспорт", "Свидетельство о рождении", "Водительское удостоверение"],
+      user: {
+        surname: '',
+        name: '',
+        patronym: '',
+        birthdate: '',
+        phone: '',
+        gender: '',
+        customerGroup: [],
+        doctor: '',
+        noSendSms: false,
+        address: {
+          postCode: '',
+          country: '',
+          area: '',
+          city: '',
+          street: '',
+          house: '',
+        },
+        document: {
+          type: '',
+          series: '',
+          number: '',
+          issuedByWhom: '',
+          dateOfIssue: '',
+        }
+      }
+    }
+  }
 }
 </script>
 
@@ -192,15 +338,15 @@ input[type="checkbox"]:checked::before,
 input[type="radio"]:checked::before
   transform: scale(1)
 
-input[type="text"]:valid,
-input[type="tel"]:valid,
-input[type="number"]:valid
-  border: solid 2px #05bc55
+// input[type="text"]:valid,
+// input[type="tel"]:valid,
+// input[type="number"]:valid
+//   border: solid 2px #05bc55
 
-input[type="text"]:invalid,
-input[type="tel"]:invalid,
-input[type="number"]:invalid
-  border: solid 2px #e63946
+// input[type="text"]:invalid,
+// input[type="tel"]:invalid,
+// input[type="number"]:invalid
+//   border: solid 2px #e63946
 
 .btn
   background: #05bc55
